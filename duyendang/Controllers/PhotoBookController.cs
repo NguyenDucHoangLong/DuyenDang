e8ced61fd61bac231841b0afcf53cdeea21e741d
loiluongit <loiluongit@gmail.com>
2016-03-05 13:22:23 +0700

    Adding data entity, PhotoBookController

---
+++ duyendang/Controllers/PhotoBookController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace duyendang.Controllers
+{
+    public class PhotoBookController : Controller
+    {
+        // GET: PhotoBook
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
