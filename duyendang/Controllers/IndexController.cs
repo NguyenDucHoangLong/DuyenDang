5b7ab851c4e2bff8fc237e09ed3c85e27efb5755
Hoang Long <ndhlong1994@gmail.com>
2016-03-06 13:40:40 +0700

    update index

--- duyendang/Controllers/IndexController.cs
+++ duyendang/Controllers/IndexController.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using duyendang.Models;
 
 namespace duyendang.Controllers
 {
     public class IndexController : Controller
     {
+        DUYENDANGDBEntities db = new DUYENDANGDBEntities();
         // GET: Index
-        public ActionResult Index()
+        public ActionResult Index() 
         {
-            return View();
+            //Trả về none nếu không thỏa, trả về hàng nếu thỏa điều kiện
+            cataloge Cataloge = db.cataloges.SingleOrDefault(c => c.id == 1);// id = 1 là home
+
+            if(Cataloge == null)
+            {
+                //Trả về trang báo lỗi
+                Response.StatusCode = 404;
+                return null;
+            }
+
+            return View(Cataloge);
+        }
+
+        //Category home partial
+        public PartialViewResult CategoryHomePartial()
+        {
+             var lstCategory = db.cataloges.Where(c => c.id > 1 && c.id <= 7).ToList();
+           // id từ Dịch Vụ PhotoBook đến Card Visit ở Home
+             return PartialView(lstCategory);
         }
     }
 }
