daf1fa541ea2caedc256927aa4c243c7a327efbe
Hoang Long <ndhlong1994@gmail.com>
2016-03-07 21:54:43 +0700

    update login

---
+++ duyendang/Controllers/AdminController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using duyendang.Models;
+using System.Data.Entity;
+
+namespace duyendang.Controllers
+{
+    public class AdminController : Controller
+    {
+        private DUYENDANGDBEntities db = new DUYENDANGDBEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CheckLogin(string username, string password)
+        {
+            user account = db.users.Where(a => a.name == username && a.password == password).FirstOrDefault();
+            if(account == null)
+            {
+                ModelState.AddModelError("Fail", "Tên đăng nhập hoặc mật khẩu không đúng!");
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                Session["user"] = username;
+                return RedirectToAction("");
+            }
+        }
+
+        public ActionResult Manager()
+        {
+            if (Session["user"] == null)
+                return RedirectToAction("Index");
+            else
+                return View();
+        }
+    }
+}
