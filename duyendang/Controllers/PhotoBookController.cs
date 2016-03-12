4aa61853e455ea49689526be6ec62170550ba102
loiluongit <loiluongit@gmail.com>
2016-03-13 02:20:30 +0700

    merge to master branch

--- duyendang/Controllers/PhotoBookController.cs
+++ duyendang/Controllers/PhotoBookController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using duyendang.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,40 @@ namespace duyendang.Controllers
 {
     public class PhotoBookController : Controller
     {
+        DUYENDANGDBEntities db = new DUYENDANGDBEntities();
         // GET: PhotoBook
         public ActionResult Index()
         {
-            ViewBag.Title = "PhotoBook";
-            return View();
+            var lst = db.information.Where(c => c.catalogeId == 2).ToList();
+            return View(lst);
+        }
+
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            var lst = db.information.Where(c => c.catalogeId == 2).ToList();
+            return View(lst);
+        }
+            
+        [HttpPost]
+        public ActionResult Edit(FormCollection formContent, HttpPostedFileBase[] file)
+        {
+            var lst = db.information.Where(c => c.catalogeId == 2).ToList();
+            
+            for (int i = 0; i < lst.Count; i++)
+            {
+                if (file[i] != null && file[i].ContentLength > 0)
+                {
+                    string filename = System.IO.Path.GetFileName(file[i].FileName);
+                    string path = Server.MapPath("~/Images/" + filename);
+                    file[i].SaveAs(path);
+                    lst[i].image = filename;
+                }
+                String nameDesc = lst[i].id.ToString();
+                lst[i].description = formContent[nameDesc];
+            }
+            db.SaveChanges();
+            return RedirectToAction("Edit");
         }
     }
 }
